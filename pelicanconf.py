a66266e2ff43b01082dd44ca00e7b5e1199f48b4
kennedy <mutemikennedy@gmail.com>
2016-05-08 16:09:55 +0300

    Add blogroll links in config file

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,10 +20,9 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Home', 'http://kennedymutemi.com/home.html'),
+         ('About', 'http://kennedymutemi.com/about.html'),
+         ('Projects', 'http://kennedymutemi.com/projects.html'),)
 
 # Social widget
 SOCIAL = (('@quizonia', 'https://twitter.com/quizonia/'),
